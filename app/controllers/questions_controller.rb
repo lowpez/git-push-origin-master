21c325aad05dd612852719f7b205c059c64c3bfa
lowpez <lopezaguayo@aol.com>
2021-07-20 21:37:47 -0400

    Stupid Coaching + Lewagon listo

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,17 +1,17 @@
-class QuestionsController < ActionController::Base
-
+class QuestionsController < ApplicationController
   def ask
   end
 
   def answer
-    @answer = ""
-    if params[:answer].downcase == "i am going to work right now!"
-      @answer =  "Great!"
-    elsif params[:answer].end_with?("?")
+    @question = params[:question]
+    if @question.blank?
+      @answer = "I can't hear you!"
+    elsif @question =~ /i am going to work/i
+      @answer = "Great!"
+    elsif @question.ends_with?("?")
       @answer = "Silly question, get dressed and go to work!"
     else
       @answer = "I don't care, get dressed and go to work!"
     end
   end
-
 end
